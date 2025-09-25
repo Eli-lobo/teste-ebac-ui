33cbbee510add50c2c1fab96acfa23146081df74
Eli <elilobonaniquinha@outlook.com>
2025-09-25 07:23:51 -0300

    Produtos

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade: Produtos', () => {
+    beforeEach(() => {
+         cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.products > .row')
+        
+        // o first() seleciona o primeiro item da lista
+        // o last() seleciona o último item da lista
+        // o eq(3) seleciona o item da posição 3 da lista, começando do 0
+        // o contains('Abominable Hoodie') seleciona o item que contém o texto 'Abominable Hoodie'
+            //.first()
+           // .last()
+           //  .eq(3)
+           .contains('Abominable Hoodie')
+            .click()
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    });
+
+});
